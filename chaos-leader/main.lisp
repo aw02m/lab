24b9722177a071c66e6676d5108d06adef444c2f
aw02m <amoh8192@gmail.com>
2018-05-22 15:01:25 +0900

    add chaos-leader

--- chaos-leader/main.lisp
+++ chaos-leader/main.lisp
@@ -3,12 +3,13 @@
 (require 'cl-glut)
 (require 'cl-glu)
 
-(load "runge-kutta.lisp")
+(load "mk-rkstep.lisp")
 (load "func.lisp")
 
 (defvar *width* 800)
 (defvar *height* 800)
 
+;; class : Window
 (defclass Window (glut:window)
   ()
   (:default-initargs
@@ -18,14 +19,15 @@
 
 ;; init
 (defmethod glut:display-window :before ((window Window))
-  (gl:clear-color 1 1 1 1))
+  (gl:clear-color 1 1 1 1)
+  (gl:clear :color-buffer-bit)) ; clear buffer
 
 ;; view port
 (defmethod glut:reshape ((window Window) width height)
   (gl:viewport 0 0 width height)
   (gl:load-identity)
   (gl:ortho (- (/ width *width*)) (/ width *width*) (- (/ height *height*)) (/ height *height*) -1 1))
-  ;(gl:translate (/ *width* 2) (/ *height* 2) 0))
+                                        ;(gl:translate (/ *width* 2) (/ *height* 2) 0))
 
 ;; keyboard
 (defmethod glut:keyboard ((window Window) key x y)
@@ -34,21 +36,24 @@
     ((#\q)
      (glut:destroy-current-window))))
 
+;; idle
+(defmethod glut:idle ((window Window))
+  (sleep (/ 1.0 1000.0))
+  (glut:post-redisplay))
+
 ;; display
-(defmethod glut:display ((window Window))
-  (gl:clear :color-buffer-bit) ; clear buffer
+(let ((rkstep (mk-rkstep `(,#'f ,#'g) '(2 2) 0 0.01)))
+  (defmethod glut:display ((window Window))
+    (let ((rkvalue (funcall rkstep)))
+      (%gl:color-3d 0.4 0.6 0.8) ; line color
 
-  (%gl:color-3d 0.4 0.6 0.8) ; line color
-  
-  (gl:begin :line-strip) ; line-strip : line with no finishing clozure
-  ;; runge-kutta loop with lines
-  (loop for r in (runge-kutta (list f g) ; list of function
-                              (list 0 1) ; list of parameter
-                              0 0.01 10000) ; t0 h loops
-     do (%gl:vertex-2d (* 0.15 (cadr r)) (* 0.15 (caddr r)))) ; pick x y from r
-  (gl:end)
-  
-  (gl:flush)) ; show window
+      (gl:begin :line-strip) ; line-strip : line with no finishing clozure
+      (%gl:vertex-2d (* 0.15 (caar rkvalue)) (* 0.15 (cadar rkvalue)))
+      (%gl:vertex-2d (* 0.15 (caadr rkvalue)) (* 0.15 (cadadr rkvalue)))
+      (gl:end)
 
+      (gl:flush)))) ; show window
+
+;; main
 (defun main ()
   (glut:display-window (make-instance 'Window)))
---
+++ chaos-leader/mk-rkstep.lisp
@@ -0,0 +1,50 @@
+;;;
+;;; Return one step function of rk4-method
+;;; Included clozure
+;;;
+(defun mk-rkstep (f-list x-list ti h)
+  (let* ((n (length f-list))
+         (f (make-array n :initial-contents f-list))
+         (x (make-array n :initial-contents x-list))
+         (k (make-array `(4 ,n) :element-type 'double-float :initial-element 0d0))
+         (temp (make-array n :element-type 'double-float :initial-element 0d0))
+         (prex (make-array n :initial-contents x-list)))
+    ;; change type to double-float
+    (loop for i from 0 to (1- n) do
+         (setf h (float h 1d0)
+               ti (float ti 1d0)
+               (aref x i) (float (aref x i) 1d0)
+               (aref prex i) (float (aref prex i) 1d0)))
+    ;; clozure
+    (lambda ()
+      (setf (aref prex 0) (aref x 0)
+            (aref prex 1) (aref x 1))
+      ;; k0
+      (loop for j from 0 to (1- n) do
+           (setf (aref k 0 j) (funcall (aref f j) ti x))
+           (setf (aref temp j) (+ (aref x j) (/ (* h (aref k 0 j)) 2))))
+      ;; k1
+      (loop for j from 0 to (1- n) do
+           (setf (aref k 1 j) (funcall (aref f j) (+ ti (/ h 2)) temp)))
+      (loop for j from 0 to (1- n) do
+           (setf (aref temp j) (+ (aref x j) (/ (* h (aref k 1 j)) 2))))
+      ;; k2
+      (loop for j from 0 to (1- n) do
+           (setf (aref k 2 j) (funcall (aref f j) (+ ti (/ h 2)) temp)))
+      (loop for j from 0 to (1- n) do
+           (setf (aref temp j) (+ (aref x j) (* h (aref k 2 j)))))
+      ;; k3
+      (loop for j from 0 to (1- n) do
+           (setf (aref k 3 j) (funcall (aref f j) (+ ti h) temp)))
+      ;; x-vector
+      (loop for j from 0 to (1- n) do
+           (setf (aref x j) (+ (aref x j)
+                               (/ (* (+ (aref k 0 j)
+                                        (* 2 (aref k 1 j))
+                                        (* 2 (aref k 2 j))
+                                        (aref k 3 j))
+                                     h)
+                                  6))))
+      (setf ti (+ ti h))
+      ;; return values
+      `((,(aref prex 0) ,(aref prex 1)) (,(aref x 0) ,(aref x 1))))))
